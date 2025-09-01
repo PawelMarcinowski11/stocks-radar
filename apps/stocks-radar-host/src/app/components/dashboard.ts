35a1d2eca7d0d4734a73fec6cb61e9dfafb989ed
Paweł Marcinowski <pawel.marcinowski11@gmail.com>
2025-09-01 10:33:50 +0200

    restructure table as a lib instead of an app

--- apps/stocks-radar-host/src/app/components/dashboard.ts
+++ apps/stocks-radar-host/src/app/components/dashboard.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Table } from '@stocks-radar/table';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [Table],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
+  standalone: true,
 })
 export class Dashboard {}
--- libs/table/eslint.config.mjs
+++ libs/table/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: 'lib',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'lib',
           style: 'kebab-case',
         },
       ],
